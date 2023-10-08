cc8ec4442c5f389afceb0f210642f7f89a5dc364
Monikaverma2001 <monika8427084@gmail.com>
2023-10-08 10:47:57 +0530

    insert operation but norule error behind realm

--- app/src/main/java/com/example/gndectouch/MainActivity.java
+++ app/src/main/java/com/example/gndectouch/MainActivity.java
@@ -9,18 +9,26 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.mongodb.lang.NonNull;
+import org.bson.Document;
 
 import io.realm.Realm;
 import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
 import io.realm.mongodb.Credentials;
 import io.realm.mongodb.User;
+import io.realm.mongodb.mongo.MongoClient;
+import io.realm.mongodb.mongo.MongoCollection;
+import io.realm.mongodb.mongo.MongoDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
+    MongoDatabase mongoDatabase;
+    MongoClient mongoClient;
+    //real mongodb
+
     String Appid="application-0-kdmkx";
-    String url="mongodb+srv://monika8427084:123@cluster0.r7gdsrb.mongodb.net/";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,25 +40,13 @@ public class MainActivity extends AppCompatActivity {
         password=findViewById(R.id.passinput);
         // TODO Auto-generated method stub
 
-        //real mongodb
         Realm.init(this);
-
-        // MongoClient mongoClient = (MongoClient) MongoClients.create(url);
-        // MongoDatabase database = mongoClient.getDatabase("GNDECdb");
+        App app=new App(new AppConfiguration.Builder(Appid).build());
 
         Button guest=findViewById(R.id.guest);
-        guest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-             //   MongoCollection<Document> collection = database.getCollection("Faculity");
 
-                // Create a document to insert
-               // Document document = new Document("email","luch@mail.com");
 
-                // Insert the document
-               // collection.insertOne(document);
-            }
-        });
+//login
         log.setOnClickListener(
                 new View.OnClickListener() {
 
@@ -58,28 +54,82 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
 
                         //instance of realm which is online placed
-                        App app=new App(new AppConfiguration.Builder(Appid).build());
+
                         //part of credential
                         String e=email.getText().toString();
-                        String p=password.getText().toString();
+                       String p=password.getText().toString();
                         //mongodb link 1. connect mongodb+srv://monika8427084:123@cluster0.r7gdsrb.mongodb.net/
                         //step2. click()-->
                         // -->admin mail--> if faculity --password --
                         //                      --> id else mentee
                         //                       -->if else mentor
                         //                       --> else(without login)
-
-                        Credentials credentials=Credentials.emailPassword(e,p);
-                        app.loginAsync(credentials, new App.Callback<User>() {
+                       // Credentials credentials=Credentials.anonymous();
+                       Credentials credentials=Credentials.emailPassword("monika8427084@gmail.com","Monika8427@#");
+//                       app.loginAsync(Credentials.anonymous(), new App.Callback<User>() {
+//                           @Override
+//                           public void onResult(App.Result<User> result) {
+//                               Toast.makeText(MainActivity.this, "HELLO MONIKA 1", Toast.LENGTH_SHORT).show();
+//
+//                                   if(result.isSuccess())
+//                                   {
+//
+//                                       User user=app.currentUser();
+//                                       Toast.makeText(MainActivity.this, user.getId().toString(), Toast.LENGTH_SHORT).show();
+//                                       mongoClient=user.getMongoClient("mongodb-atlas");
+//                                       mongoDatabase=mongoClient.getDatabase("GNDECdb");
+//                                       //mywebsite link so they can firstly recover their password from website
+////                                    startActivity(intent);
+//
+//                                       MongoCollection<Document> mongoCollection=mongoDatabase.getCollection("Faculity");
+//
+//                                       Toast.makeText(MainActivity.this, mongoCollection.find().toString(), Toast.LENGTH_SHORT).show();
+//                                       System.out.println(mongoCollection.find().toString());
+//
+//                                       mongoCollection.insertOne(new Document("userId",user.getId()).append("data","hello dude")).getAsync(result1 -> {
+//                                           if(result1.isSuccess())
+//                                           {
+//                                               Toast.makeText(MainActivity.this, "HELLO MONIKA", Toast.LENGTH_SHORT).show();
+//                                           }
+//                                           else {
+//                                               Toast.makeText(MainActivity.this, "dont allow", Toast.LENGTH_SHORT).show();
+//                                           }
+//                                       });
+//
+//                                       Intent intent=new Intent(MainActivity.this,MainActivity2log.class);
+//                                       startActivity(intent);
+//                                   }
+//                                   else{
+//                                       Toast.makeText(MainActivity.this, "CHECK INTERNET", Toast.LENGTH_SHORT).show();
+//                                   }
+//                               }
+//
+//                       });
+                          app.loginAsync(credentials, new App.Callback<User>() {
                             @Override
-                            public void onResult(@NonNull App.Result<User> result) {
+                            public void onResult( App.Result<User> result) {
                                 if(result.isSuccess())
                                 {
 
+                                    User user=app.currentUser();
+                                    mongoClient=user.getMongoClient("mongodb-atlas");
+                                    mongoDatabase=mongoClient.getDatabase("GNDECdb");
                                     //mywebsite link so they can firstly recover their password from website
 //                                    startActivity(intent);
-                                     Intent intent=new Intent(MainActivity.this,MainActivity2log.class);
-                                     startActivity(intent);
+
+                                    MongoCollection<Document> mongoCollection=mongoDatabase.getCollection("Faculity");
+                                    mongoCollection.insertOne(new Document("userid",user.getId()).append("data","hello dude")).getAsync(result1 -> {
+                                        if(result1.isSuccess())
+                                        {
+                                            Toast.makeText(MainActivity.this, "HELLO MONIKA", Toast.LENGTH_SHORT).show();
+                                        }
+                                        else {
+                                            Toast.makeText(MainActivity.this, "dont allow", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+
+                                    Intent intent=new Intent(MainActivity.this,MainActivity2log.class);
+                                    startActivity(intent);
                                 }
                                 else{
                                      Toast.makeText(MainActivity.this, e+" "+p+"CHECK INTERNET", Toast.LENGTH_SHORT).show();
@@ -93,7 +143,22 @@ public class MainActivity extends AppCompatActivity {
 //                       finish();
                     }
                 }
+
+
+
         );
+
+
+
+
+
+//login as guest
+        guest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //code
+            }
+        });
 
     }
 
