fa4cbdfdfe8f94c801c066ec86ae37c30d0c306e
Monikaverma2001 <monika8427084@gmail.com>
2023-10-11 21:08:37 +0530

    initialize all activity but need to write code

---
+++ app/src/main/java/com/example/gndectouch/FaculityActivity.java
@@ -0,0 +1,14 @@
+package com.example.gndectouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaculityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculity);
+    }
+}
---
+++ app/src/main/java/com/example/gndectouch/MentorActivity.java
@@ -0,0 +1,14 @@
+package com.example.gndectouch;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MentorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentor);
+    }
+}
