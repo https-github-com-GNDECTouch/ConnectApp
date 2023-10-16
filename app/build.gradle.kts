7be794545a7d3a554c11d9c59ac890ad3461eebe
Jagjit Singh <jagjit.2626@gmail.com>
2023-10-16 13:30:20 +0530

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     //apply plugin of realm
     id("realm-android")
 }
-
 //for mongodb realmio
 //apply
 realm{
@@ -17,8 +16,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.gndectouch"
-        minSdk = 16
-        targetSdk = 33
+        minSdk= 19
+        targetSdk= 31
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +36,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        mlModelBinding=true
     }
 }
 
@@ -48,4 +48,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0");
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0");
 }
