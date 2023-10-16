355a1810cf25dc858228a00bcafee6914645bca8
Monikaverma2001 <monika8427084@gmail.com>
2023-10-16 20:20:18 +0530

    show data but app crash no worry i comment out the ttrouble code

--- app/src/main/java/com/example/gndectouch/FaculityActivity.java
+++ app/src/main/java/com/example/gndectouch/FaculityActivity.java
@@ -1,254 +1,239 @@
 package com.example.gndectouch;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FaculityActivity extends AppCompatActivity {
     TextView mentorname;
     private ActivityResultLauncher<Intent> filePickerLauncher;
     private ActivityResultLauncher<Intent> filePickerLauncher1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculity);
-
-        LinearLayout whole=(LinearLayout) findViewById(R.id.linearLayout);
-
-        mentorname=(TextView)findViewById(R.id.mentorname);
-        Intent intent = getIntent();
-        ArrayList<String> mentorlist = (ArrayList<String> )intent.getStringArrayListExtra("data");
-
-        // Now you can use the mentorlist in this activity
-        if (mentorlist != null) {
-            for (String name : mentorlist ) {
-                // Do something with each name
-                if(name!=null)
-                      Toast.makeText(this, "hello ji "+name, Toast.LENGTH_SHORT).show();
-            }
-        }
-        //Toast.makeText(this, mentorlist.size(), Toast.LENGTH_SHORT).show();
-       // mentorname.setText(builder.toString());
-
-
-        // Now you can use the mentorlist in this activity
-
-
-        List<Student> books = readBooksFromCSV("books.txt");
-
-        //take student csv
-        Button btn=findViewById(R.id.selectFileButton);
-        Button btn1=findViewById(R.id.selectFileButton1);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFilePicker1();
-            }
-
-
-        });
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFilePicker();
-            }
-
-
-        });
-
-
-        filePickerLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                        Uri selectedFileUri = result.getData().getData();
-                        if (selectedFileUri != null) {
-                            displayFileContent(selectedFileUri);
-                        }
-
-                        // Handle the selected file URI (e.g., upload it to a server).
-                        // For this example, we are not implementing the file upload.
-                    }
-                }
-        );
-        filePickerLauncher1 = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                        Uri selectedFileUri = result.getData().getData();
-                        if (selectedFileUri != null) {
-                            displayFileContent1(selectedFileUri);
-                        }
-
-                        // Handle the selected file URI (e.g., upload it to a server).
-                        // For this example, we are not implementing the file upload.
-                    }
-                }
-        );
-
     }
-    private void displayFileContent(Uri fileUri) {
-        try {
-            InputStream inputStream = getContentResolver().openInputStream(fileUri);
-            if (inputStream != null) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                StringBuilder content = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    content.append(line).append('\n');
-                }
-                inputStream.close();
-                TextView data=findViewById(R.id.csvdata);
-                data.setText(content.toString());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    private void displayFileContent1(Uri fileUri) {
-        try {
-            InputStream inputStream = getContentResolver().openInputStream(fileUri);
-            if (inputStream != null) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                StringBuilder content = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    content.append(line).append('\n');
-                }
-                inputStream.close();
-                TextView data=findViewById(R.id.csvdata1);
-                data.setText(content.toString());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    private void openFilePicker() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
-        filePickerLauncher.launch(intent);
-    }
-    private void openFilePicker1() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
-        filePickerLauncher1.launch(intent);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            if (data != null) {
-                Toast.makeText(FaculityActivity.this, "HELLO MONIKA 1", Toast.LENGTH_SHORT).show();
-                Uri selectedFileUri = data.getData();
-                TextView csv=findViewById(R.id.csvdata);
-                csv.setText(selectedFileUri.toString());
-                // You can use the selected file URI to access the chosen file.
-                // For example, you can open and read the file's contents.
-            }
-            else{
-                Toast.makeText(FaculityActivity.this, "Null data", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-
-
-
-
-//selectFileButton.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            // Start an Intent to open the file picker
-//            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-//            intent.addCategory(Intent.CATEGORY_OPENABLE);
-//            intent.setType("*/*");  // You can specify a MIME type if you want to filter file types.
-//
-//            startActivityForResult(intent, 1); // Use a unique request code
-//        }
-//    });
-//
-//
-
-
-
-
-
-
-
-
-
-    private static List<Student> readBooksFromCSV(String fileName) {
-        List<Student> books = new ArrayList<>();
-        Path pathToFile = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            pathToFile = Paths.get(fileName);
-        }
-
-        // create an instance of BufferedReader
-        // using try with resource, Java 7 feature to close resources
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            try (BufferedReader br = Files.newBufferedReader(pathToFile,
-                    StandardCharsets.US_ASCII)) {
-
-                // read the first line from the text file
-                String line = br.readLine();
-
-                // loop until all lines are read
-                while (line != null) {
-
-                    // use string.split to load a string array with the values from
-                    // each line of
-                    // the file, using a comma as the delimiter
-                    String[] attributes = line.split(",");
-//
-//                    Student book = createBook(attributes);
-//
-//                    // adding book into ArrayList
-//                    books.add(book);
-
-                    // read next line before looping
-                    // if end of file reached, line would be null
-                    line = br.readLine();
-                }
-
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-        }
-
-        return books;
-    }
-
-    private static Student createBook(String[] metadata) {
-        String name = metadata[0];
-        int price = Integer.parseInt(metadata[1]);
-        String author = metadata[2];
-
-        // create and return book of this metadata
-        return new Student();
-    }
-
-
 }
+
+//        LinearLayout whole=(LinearLayout) findViewById(R.id.linearLayout);
+//
+//        mentorname=(TextView)findViewById(R.id.mentorname);
+//        Intent intent = getIntent();
+//        ArrayList<String> mentorlist = (ArrayList<String> )intent.getStringArrayListExtra("data");
+//
+//        // Now you can use the mentorlist in this activity
+//        if (mentorlist != null) {
+//            for (String name : mentorlist ) {
+//                // Do something with each name
+//                if(name!=null)
+//                      Toast.makeText(this, "hello ji "+name, Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//        //Toast.makeText(this, mentorlist.size(), Toast.LENGTH_SHORT).show();
+//       // mentorname.setText(builder.toString());
+//
+//
+//        // Now you can use the mentorlist in this activity
+//
+//
+//        List<Student> books = readBooksFromCSV("books.txt");
+//
+//        //take student csv
+//        Button btn=findViewById(R.id.selectFileButton);
+//        Button btn1=findViewById(R.id.selectFileButton1);
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openFilePicker1();
+//            }
+//
+//
+//        });
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openFilePicker();
+//            }
+//
+//
+//        });
+//
+//
+//        filePickerLauncher = registerForActivityResult(
+//                new ActivityResultContracts.StartActivityForResult(),
+//                result -> {
+//                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+//                        Uri selectedFileUri = result.getData().getData();
+//                        if (selectedFileUri != null) {
+//                            displayFileContent(selectedFileUri);
+//                        }
+//
+//                        // Handle the selected file URI (e.g., upload it to a server).
+//                        // For this example, we are not implementing the file upload.
+//                    }
+//                }
+//        );
+//        filePickerLauncher1 = registerForActivityResult(
+//                new ActivityResultContracts.StartActivityForResult(),
+//                result -> {
+//                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+//                        Uri selectedFileUri = result.getData().getData();
+//                        if (selectedFileUri != null) {
+//                            displayFileContent1(selectedFileUri);
+//                        }
+//
+//                        // Handle the selected file URI (e.g., upload it to a server).
+//                        // For this example, we are not implementing the file upload.
+//                    }
+//                }
+//        );
+//
+//    }
+//    private void displayFileContent(Uri fileUri) {
+//        try {
+//            InputStream inputStream = getContentResolver().openInputStream(fileUri);
+//            if (inputStream != null) {
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//                StringBuilder content = new StringBuilder();
+//                String line;
+//                while ((line = reader.readLine()) != null) {
+//                    content.append(line).append('\n');
+//                }
+//                inputStream.close();
+//                TextView data=findViewById(R.id.csvdata);
+//                data.setText(content.toString());
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//    private void displayFileContent1(Uri fileUri) {
+//        try {
+//            InputStream inputStream = getContentResolver().openInputStream(fileUri);
+//            if (inputStream != null) {
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//                StringBuilder content = new StringBuilder();
+//                String line;
+//                while ((line = reader.readLine()) != null) {
+//                    content.append(line).append('\n');
+//                }
+//                inputStream.close();
+//                TextView data=findViewById(R.id.csvdata1);
+//                data.setText(content.toString());
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//    private void openFilePicker() {
+//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//        intent.setType("*/*");
+//        filePickerLauncher.launch(intent);
+//    }
+//    private void openFilePicker1() {
+//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//        intent.setType("*/*");
+//        filePickerLauncher1.launch(intent);
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == 1 && resultCode == RESULT_OK) {
+//            if (data != null) {
+//                Toast.makeText(FaculityActivity.this, "HELLO MONIKA 1", Toast.LENGTH_SHORT).show();
+//                Uri selectedFileUri = data.getData();
+//                TextView csv=findViewById(R.id.csvdata);
+//                csv.setText(selectedFileUri.toString());
+//                // You can use the selected file URI to access the chosen file.
+//                // For example, you can open and read the file's contents.
+//            }
+//            else{
+//                Toast.makeText(FaculityActivity.this, "Null data", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
+//
+//
+//
+//
+//
+////selectFileButton.setOnClickListener(new View.OnClickListener() {
+////        @Override
+////        public void onClick(View view) {
+////            // Start an Intent to open the file picker
+////            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+////            intent.addCategory(Intent.CATEGORY_OPENABLE);
+////            intent.setType("*/*");  // You can specify a MIME type if you want to filter file types.
+////
+////            startActivityForResult(intent, 1); // Use a unique request code
+////        }
+////    });
+////
+////
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//    private static List<Student> readBooksFromCSV(String fileName) {
+//        List<Student> books = new ArrayList<>();
+//        Path pathToFile = null;
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            pathToFile = Paths.get(fileName);
+//        }
+//
+//        // create an instance of BufferedReader
+//        // using try with resource, Java 7 feature to close resources
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            try (BufferedReader br = Files.newBufferedReader(pathToFile,
+//                    StandardCharsets.US_ASCII)) {
+//
+//                // read the first line from the text file
+//                String line = br.readLine();
+//
+//                // loop until all lines are read
+//                while (line != null) {
+//
+//                    // use string.split to load a string array with the values from
+//                    // each line of
+//                    // the file, using a comma as the delimiter
+//                    String[] attributes = line.split(",");
+////
+////                    Student book = createBook(attributes);
+////
+////                    // adding book into ArrayList
+////                    books.add(book);
+//
+//                    // read next line before looping
+//                    // if end of file reached, line would be null
+//                    line = br.readLine();
+//                }
+//
+//            } catch (IOException ioe) {
+//                ioe.printStackTrace();
+//            }
+//        }
+//
+//        return books;
+//    }
+//
+//    private static Student createBook(String[] metadata) {
+//        String name = metadata[0];
+//        int price = Integer.parseInt(metadata[1]);
+//        String author = metadata[2];
+//
+//        // create and return book of this metadata
+//        return new Student();
+//    }
+//
+//
+//}
--- app/src/main/java/com/example/gndectouch/alumniView.java
+++ app/src/main/java/com/example/gndectouch/alumniView.java
@@ -1,14 +1,107 @@
 package com.example.gndectouch;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.bson.Document;
+
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.RealmResultTask;
+import io.realm.mongodb.User;
+import io.realm.mongodb.mongo.MongoClient;
+import io.realm.mongodb.mongo.MongoCollection;
+import io.realm.mongodb.mongo.MongoDatabase;
+import io.realm.mongodb.mongo.iterable.MongoCursor;
 
 public class alumniView extends AppCompatActivity {
+
+    TextView mentorname;
+    MongoDatabase mongoDatabase;
+    MongoClient mongoClient;
+    String Appid = "application-0-kdmkx";
+    private ActivityResultLauncher<Intent> filePickerLauncher;
+    private ActivityResultLauncher<Intent> filePickerLauncher1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alumni_view);
+        LinearLayout whole = (LinearLayout) findViewById(R.id.linearlay);
+
+
+     //   Realm.init(this);
+
+//        real work
+//        first connect with data base and check which login belong to faculity,mentor or mentee?
+
+        App app = new App(new AppConfiguration.Builder(Appid).build());
+
+
+        app.loginAsync(Credentials.emailPassword("monika8427084@gmail.com", "Monika8427@#"), new App.Callback<User>() {
+            @Override
+            public void onResult(App.Result<User> resulting)
+            {
+
+                Toast.makeText(alumniView.this, "stap1", Toast.LENGTH_SHORT).show();
+                Document document = new Document("occ", "mentor");
+                LinearLayout linear = findViewById(R.id.linearlay);
+                User user = app.currentUser();
+                mongoClient = user.getMongoClient("mongodb-atlas");
+                mongoDatabase = mongoClient.getDatabase("GNDECdb");
+                MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("Mentor");
+                Toast.makeText(alumniView.this, "stap2 hello ", Toast.LENGTH_SHORT).show();
+                RealmResultTask<MongoCursor<Document>> mentorlist = mongoCollection.find(document).iterator();
+//                mentorlist.getAsync(task -> {
+//                    if (task.isSuccess()) {
+//                        Toast.makeText(alumniView.this, "stap2", Toast.LENGTH_SHORT).show();
+//                        MongoCursor<Document> resu = task.get();
+//
+//                        while (resu.hasNext()) {
+//                            LinearLayout echrow = (LinearLayout) findViewById(R.id.linearLayout);
+//                            Document curDoc = resu.next();
+//                            if (curDoc.getString("email") != null) {
+//                                Toast.makeText(alumniView.this, "stap3", Toast.LENGTH_SHORT).show();
+//                                //  mentorlist.add(curDoc.getString("name"));
+//                                TextView mentoremail = findViewById(R.id.emailinput);
+//                                mentoremail.setText(curDoc.getString("email"));
+//                                TextView mentorname = findViewById(R.id.mentorname);
+//                                mentorname.setText(curDoc.getString("name"));
+//                                TextView phone = findViewById(R.id.mentorphone);
+//                                phone.setText(curDoc.getString("phone"));
+//
+//                                // 'this' is the context
+//
+//
+//                                // You can customize the TextView here, e.g., set text size, text color, etc.
+//                                Toast.makeText(alumniView.this, "stap4", Toast.LENGTH_SHORT).show();
+//                                // Add the TextView to the LinearLayout
+//                                echrow.addView(mentoremail);
+//                                echrow.addView(mentorname);
+//                                echrow.addView(phone);
+//
+//                            }
+//                            Toast.makeText(alumniView.this, "stap5", Toast.LENGTH_SHORT).show();
+//                            linear.addView(echrow);
+//
+//                        }
+//                        Toast.makeText(alumniView.this, "stap6", Toast.LENGTH_SHORT).show();
+//                        ScrollView scroller = findViewById(R.id.scrollView2);
+//                        scroller.addView(linear);
+//                    }
+//                });
+            }
+        });
     }
 }
+
+
+
+//}
