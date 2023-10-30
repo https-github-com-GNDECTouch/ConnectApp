e060b4f6bad3b26c8ff8e4e41fcdccab781880b6
Jagjit Singh <jagjit.2626@gmail.com>
2023-10-30 21:49:29 +0530

    studentname

--- app/src/main/java/com/example/gndectouch/UploadData.java
+++ app/src/main/java/com/example/gndectouch/UploadData.java
@@ -29,24 +29,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the  factory method to
- * create an instance of this fragment.
- */
 public class UploadData extends Fragment {
     private ActivityResultLauncher<Intent> filePickerLauncher;
     private ActivityResultLauncher<Intent> filePickerLauncher1;
     CSVReader reader;
     CSVReader reader1;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_upload_data, container, false);
-TextView resulted=view.findViewById(R.id.resulted);
-        // Find views within the fragment's layout
+        TextView resulted = view.findViewById(R.id.resulted);
+
         Button mentorlist = view.findViewById(R.id.uploadbuttonformentor);
         Button menteelist = view.findViewById(R.id.uploadbuttonformentee);
         Button alotmentor = view.findViewById(R.id.alotmentortomentee);
@@ -59,30 +51,23 @@ TextView resulted=view.findViewById(R.id.resulted);
                     try {
                         InputStream inputStream = requireContext().getContentResolver().openInputStream(selectedFileUri);
                         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                         this.reader = new CSVReader(inputStreamReader);
-
-                        // Process the CSV data
-
+                        this.reader = new CSVReader(inputStreamReader);
                     } catch (IOException e) {
                         // Handle any IO exceptions.
                     }
-                    // Handle the selected file URI here.
                 }
             }
         });
+
         filePickerLauncher1 = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == Activity.RESULT_OK) {
                 Intent data = result.getData();
                 if (data != null) {
                     Uri selectedFileUri = data.getData();
-                    // Handle the selected file URI here.
                     try {
                         InputStream inputStream = requireContext().getContentResolver().openInputStream(selectedFileUri);
                         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                         this.reader1 = new CSVReader(inputStreamReader);
-
-                        // Process the CSV data
-
                     } catch (IOException e) {
                         // Handle any IO exceptions.
                     }
@@ -90,14 +75,12 @@ TextView resulted=view.findViewById(R.id.resulted);
             }
         });
 
-        // Perform further operations with the views
         mentorlist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("*/*");
                 filePickerLauncher.launch(intent);
-
             }
         });
 
@@ -107,7 +90,6 @@ TextView resulted=view.findViewById(R.id.resulted);
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("*/*");
                 filePickerLauncher1.launch(intent);
-
             }
         });
 
@@ -115,60 +97,70 @@ TextView resulted=view.findViewById(R.id.resulted);
             @Override
             public void onClick(View v) {
                 try {
-                List<DoublePoint> studentFeatures = new ArrayList<>();
-                List<String> teacherNames = new ArrayList<>();
-                CSVReader studentReader = reader1;
-                String[] studentRow;
-                while ((studentRow = studentReader.readNext()) != null) {
-                    double cgpa = Double.parseDouble(studentRow[3]); // Assuming CGPA is in the first column
-                    double[] point = {cgpa}; // Create a single-dimensional point
-                    studentFeatures.add(new DoublePoint(point));
-                }
-
-                // Read teacher data from teachers.csv
-                CSVReader teacherReader = reader;
-                String[] teacherRow;
-                while ((teacherRow = teacherReader.readNext()) != null) {
-                    teacherNames.add(teacherRow[1]); // Assuming teacher names are in the first column
-                }
-
-                // Number of clusters (teachers)
-                int numClusters =3;
-
-                // Perform K-Means clustering
-                KMeansPlusPlusClusterer<DoublePoint> clusterer = new KMeansPlusPlusClusterer<>(numClusters, -1, new EuclideanDistance());
-                List<CentroidCluster<DoublePoint>> clusters = clusterer.cluster(studentFeatures);
-
-                // Display cluster assignments
-                HashMap<String, List<Double>> teacherAssignments = new HashMap<>();
-                for (int i = 0; i < clusters.size(); i++) {
-                    CentroidCluster<DoublePoint> cluster = clusters.get(i);
-                    String teacherName = teacherNames.get(i);
-                    List<Double> assignments = new ArrayList<>();
-                    for (DoublePoint point : cluster.getPoints()) {
-                        assignments.add(point.getPoint()[0]); // CGPA value
+                    List<DoublePoint> studentFeatures = new ArrayList<>();
+                    List<String> studentNames = new ArrayList<>();
+                    CSVReader studentReader = reader1;
+                    String[] studentRow;
+                    boolean skipStudentHeader = true;
+                    while ((studentRow = studentReader.readNext()) != null) {
+                        if (skipStudentHeader) {
+                            skipStudentHeader = false;
+                            continue; // Skip the header row in the student CSV
+                        }
+                        double cgpa = Double.parseDouble(studentRow[3]); // Assuming CGPA is in the fourth column
+                        double[] point = { cgpa }; // Create a single-dimensional point
+                        studentFeatures.add(new DoublePoint(point));
+                        studentNames.add(studentRow[1]); // Assuming student names are in the second column
                     }
-                    teacherAssignments.put(teacherName, assignments);
-                }
-                    Toast.makeText(requireContext(), "In try", Toast.LENGTH_SHORT).show();
-                // Display the assignments
-                StringBuilder result = new StringBuilder();
-                for (String teacher : teacherAssignments.keySet()) {
-                    result.append("Teacher: ").append(teacher).append("\n");
-                    for (Double cgpa : teacherAssignments.get(teacher)) {
-                        result.append("  Student: ").append("Student Name").append(", CGPA: ").append(cgpa).append("\n");
 
+                    CSVReader teacherReader = reader;
+                    String[] teacherRow;
+                    List<String> teacherNames = new ArrayList<>();
+                    boolean skipTeacherHeader = true;
+                    while ((teacherRow = teacherReader.readNext()) != null) {
+                        if (skipTeacherHeader) {
+                            skipTeacherHeader = false;
+                            continue; // Skip the header row in the teacher CSV
+                        }
+                        teacherNames.add(teacherRow[1]); // Assuming teacher names are in the second column
                     }
-                }
+
+                    int numClusters = teacherNames.size(); // Number of clusters based on the number of teachers
+
+                    KMeansPlusPlusClusterer<DoublePoint> clusterer = new KMeansPlusPlusClusterer<>(numClusters, -1, new EuclideanDistance());
+                    List<CentroidCluster<DoublePoint>> clusters = clusterer.cluster(studentFeatures);
+
+                    HashMap<String, List<Double>> teacherAssignments = new HashMap<>();
+                    for (int i = 0; i < clusters.size(); i++) {
+                        CentroidCluster<DoublePoint> cluster = clusters.get(i);
+                        String teacherName = teacherNames.get(i);
+                        List<Double> assignments = new ArrayList<>();
+                        for (DoublePoint point : cluster.getPoints()) {
+                            assignments.add(point.getPoint()[0]); // CGPA value
+                        }
+                        teacherAssignments.put(teacherName, assignments);
+                    }
+
+                    // Display the assignments
+                    StringBuilder result = new StringBuilder();
+                    for (String teacher : teacherAssignments.keySet()) {
+                        result.append("Teacher: ").append(teacher).append("\n");
+                        List<Double> cgpaList = teacherAssignments.get(teacher);
+                        List<String> studentNameList = studentNames.subList(0, cgpaList.size()); // Get the corresponding student names
+                        for (int i = 0; i < cgpaList.size(); i++) {
+                            String studentName = studentNameList.get(i);
+                            double cgpa = cgpaList.get(i);
+                            result.append("  Student: ").append(studentName).append(", CGPA: ").append(cgpa).append("\n");
+                        }
+                    }
                     resulted.setText(result.toString());
-            }catch (Exception e) {
-                    Toast.makeText(requireContext(), e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+
         return view;
-
     }
-
-
 }
