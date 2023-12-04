93732ee735adc92165b4aa465f712fa16aac03e0
Monikaverma2001 <monika8427084@gmail.com>
2023-12-04 13:12:19 +0530

    animation applied in splash screen

--- app/src/main/java/com/example/gndectouch/UploadData.java
+++ app/src/main/java/com/example/gndectouch/UploadData.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +17,15 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import org.apache.commons.math3.ml.clustering.CentroidCluster;
 import org.apache.commons.math3.ml.clustering.DoublePoint;
 import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer;
 import org.apache.commons.math3.ml.distance.EuclideanDistance;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -37,11 +41,19 @@ public class UploadData extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_upload_data, container, false);
-        TextView resulted = view.findViewById(R.id.resulted);
+
+
+        TextView mentorcsv = view.findViewById(R.id.mentorcsv);
+        TextView studentcsv = view.findViewById(R.id.studentcsv);
+        TextView output=view.findViewById(R.id.output);
 
         Button mentorlist = view.findViewById(R.id.uploadbuttonformentor);
         Button menteelist = view.findViewById(R.id.uploadbuttonformentee);
         Button alotmentor = view.findViewById(R.id.alotmentortomentee);
+
+
+        //download file after mentor alotment
+
 
         filePickerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == Activity.RESULT_OK) {
@@ -52,12 +64,15 @@ public class UploadData extends Fragment {
                         InputStream inputStream = requireContext().getContentResolver().openInputStream(selectedFileUri);
                         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                         this.reader = new CSVReader(inputStreamReader);
+                       mentorcsv.setText("mentor.csv");
                     } catch (IOException e) {
                         // Handle any IO exceptions.
                     }
                 }
             }
         });
+
+
 
         filePickerLauncher1 = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == Activity.RESULT_OK) {
@@ -68,6 +83,7 @@ public class UploadData extends Fragment {
                         InputStream inputStream = requireContext().getContentResolver().openInputStream(selectedFileUri);
                         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                         this.reader1 = new CSVReader(inputStreamReader);
+                       studentcsv.setText("student.csv");
                     } catch (IOException e) {
                         // Handle any IO exceptions.
                     }
@@ -143,18 +159,57 @@ public class UploadData extends Fragment {
 
                     // Display the assignments
                     StringBuilder result = new StringBuilder();
-                    for (String teacher : teacherAssignments.keySet()) {
-                        result.append("Teacher: ").append(teacher).append("\n");
-                        List<Double> cgpaList = teacherAssignments.get(teacher);
-                        List<String> studentNameList = studentNames.subList(0, cgpaList.size()); // Get the corresponding student names
-                        for (int i = 0; i < cgpaList.size(); i++) {
-                            String studentName = studentNameList.get(i);
-                            double cgpa = cgpaList.get(i);
-                            result.append("  Student: ").append(studentName).append(", CGPA: ").append(cgpa).append("\n");
+                    File downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                    File outputFile = new File(downloadDir, "studentdata.csv");
+
+                    CSVWriter writer = new CSVWriter(new FileWriter(outputFile));
+                    try {
+
+                        for (String teacher : teacherAssignments.keySet()) {
+                            List<String[]> data = new ArrayList<>();
+
+                            List<Double> cgpaList = teacherAssignments.get(teacher);
+                            List<String> studentNameList = studentNames.subList(0, cgpaList.size());
+
+                            for (int i = 0; i < cgpaList.size(); i++) {
+                                String studentName = studentNameList.get(i);
+                                double cgpa = cgpaList.get(i);
+                                data.add(new String[]{teacher, studentName, Double.toString(cgpa)});
+                            }
+
+                            // Write data to the CSV file without overwriting existing content
+                            writer.writeAll(data);
+
                         }
-                        result.append("\n\n");
+                        mentorcsv.setText("");
+                        studentcsv.setText("");
+
+                        output.setText("outputFile.getPath().toString()");
+// Close the writer to ensure changes are flushed to the file
+                        writer.close();
+                        Toast.makeText(requireContext(), "check student.csv file in downloaded section "+outputFile.getPath().toString(), Toast.LENGTH_SHORT).show();
+
+
+//                        resulted.setText(result.toString());
+//
+//                        List<String[]> data = new ArrayList<String[]>();
+//
+//                        data.add(new String[]{"India", "New Delhi"});
+//                        data.add(new String[]{"United States", "Washington D.C"});
+//                        data.add(new String[]{"Germany", "Berlin"});
+//
+//                        writer.writeAll(data); // data is adding to csv
+
+                    } catch (IOException e) {
+                        Toast.makeText(requireContext(), "Upload both files", Toast.LENGTH_SHORT).show();
+                        mentorcsv.setText("*");
+                        studentcsv.setText("*");
+                        alotmentor.setClickable(true);
+                        output.setText("");
                     }
-                    resulted.setText(result.toString());
+
+
+
                 } catch (Exception e) {
                     Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
@@ -163,5 +218,23 @@ public class UploadData extends Fragment {
 
 
         return view;
+    }
+    private void downloadFile() {
+        File file = new File(getContext().getExternalFilesDir(null), "studentdata.csv");
+
+        // Check if the file exists
+        if (file.exists()) {
+            // Create a DownloadManager.Request with the file Uri
+            try {
+                // Request WRITE_EXTERNAL_STORAGE permission
+
+
+            }catch (Exception e){
+                Toast.makeText(requireContext(), "File not found!    "+e, Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            Toast.makeText(requireContext(), "File not found!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
